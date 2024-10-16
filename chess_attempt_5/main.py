c8f9a0740219cbae4f1962b6883ee59f0f7094ee
Foivos Chnaras <foivoshn@gmail.com>
2024-10-16 14:31:17 -0400

    analyzed some games

--- chess_attempt_5/main.py
+++ chess_attempt_5/main.py
@@ -235,7 +235,7 @@ if __name__ == "__main__":
         exit(1)
     
     # Define the list of specific PGN files directly
-    specific_pgn_files = [f"utf8_games/twic{num}.pgn" for num in range(1556, 1560 + 1)]
+    specific_pgn_files = [f"utf8_games/twic{num}.pgn" for num in range(1540, 1549 + 1)]
     #specific_pgn_files = ["utf8_games/example20.pgn"]
     
     # Specify the output directory for analyzed CSV files
